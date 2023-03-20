2bc6f9a34e2b36e806424f8b61fa09c50e5588a6
Susana Gonzales <3777305990101@ingenieria.usac.edu.gt>
2023-03-19 21:17:29 -0600

    conectando a mysql workbrench

--- Reporte4/database/backend/src/app.js
+++ Reporte4/database/backend/src/app.js
@@ -8,6 +8,23 @@ require("dotenv").config();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
+//DB
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "root",
+   database: "proyecto",
+});
+
+connection.connect((error) => {
+   if (error) {
+      console.error("Error de conexión: " + error.stack);
+      return;
+   }
+   console.log("Conectado a la base de datos MySQL.");
+});
 
 
 module.exports = app;
