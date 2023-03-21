952284654eb34b3b0a2946ab661456ca494ca2a8
Susana Gonzales <3777305990101@ingenieria.usac.edu.gt>
2023-03-20 21:18:25 -0600

    creando todos los endpoints

--- Reporte4/database/backend/src/app.js
+++ Reporte4/database/backend/src/app.js
@@ -26,5 +26,64 @@ connection.connect((error) => {
    console.log("Conectado a la base de datos MySQL.");
 });
 
+// ROUTES
+app.get("/", (req, res) => {
+   datos.forEach((curso) => {
+      const sql = `INSERT INTO curso (codigo, nombre, creditos) VALUES ('${curso.codigo}', '${curso.nombre}', ${curso.creditos})`;
+      connection.query(sql, (error, results) => {
+         if (error) throw error;
+         console.log(`Curso ${curso.name} insertado correctamente`);
+      });
+   });
+
+   res.status(200).json({
+      msg: "Practicas Iniciales",
+   });
+});
+
+app.post("/login", (req, res) => {
+   const { registro_academico, password } = req.body;
+   const sql = `SELECT * FROM usuario WHERE registro_academico = ${registro_academico} AND password = '${password}'`;
+   connection.query(sql, (error, results) => {
+      if (error) throw error;
+
+      if (results.length > 0) {
+         res.status(200).json({
+            msg: "Usuario y contraseña correctos",
+            registro_academico: results[0].registro_academico,
+            nombres: results[0].nombres,
+            correo: results[0].correo,
+            // password: results[0].password,
+            apellidos: results[0].apellidos,
+         });
+      } else {
+         res.status(401).json({
+            msg: "Usuario y contraseña incorrectos",
+         });
+      }
+   });
+});
+
+app.post("/registro", (req, res) => {
+   const { registro_academico, nombres, apellidos, password, correo } =
+      req.body;
+   const sql = `INSERT INTO usuario (registro_academico, password, nombres, apellidos, correo) VALUES (${registro_academico}, '${password}', '${nombres}', '${apellidos}', '${correo}')`;
+
+   connection.query(
+      sql,
+      [registro_academico, password, nombres, apellidos, correo],
+      (error, results, fields) => {
+         if (error) {
+            return res.status(401).json({
+               msg: "No se ha podido registrar, registro academico ya existe",
+            });
+         }
+         res.status(200).json({
+            msg: "Se ha registrado exitosamente",
+         });
+      }
+   );
+});
+
 
 module.exports = app;
